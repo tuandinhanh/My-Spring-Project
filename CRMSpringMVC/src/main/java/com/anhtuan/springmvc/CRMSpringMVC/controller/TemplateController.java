16b19597aed6545ebdac3fe9c840c61b12c96338
tuandinhanh <dinhanhtuantuan@gmail.com>
2018-05-28 14:37:47 +0700

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # Explicit paths specified without -i or -o; assuming --only paths... # On branch master # Your branch is up-to-date with 'origin/master'. # # Changes to be committed: #       modified:   CRMSpringMVC/src/main/java/com/anhtuan/springmvc/CRMSpringM$ #       modified:   CRMSpringMVC/src/main/resources/application.properties #       modified:   CRMSpringMVC/src/main/resources/templates/home.html

--- CRMSpringMVC/src/main/java/com/anhtuan/springmvc/CRMSpringMVC/controller/TemplateController.java
+++ CRMSpringMVC/src/main/java/com/anhtuan/springmvc/CRMSpringMVC/controller/TemplateController.java
@@ -1,18 +1,25 @@
 package com.anhtuan.springmvc.CRMSpringMVC.controller;
 
+import com.anhtuan.springmvc.CRMSpringMVC.dao.UserJpaRepository;
+import com.anhtuan.springmvc.CRMSpringMVC.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.List;
+
 @Controller
 public class TemplateController {
+
+    @Autowired
+    UserJpaRepository userJpaRepository;
 
     @RequestMapping(value = "/")
     public String home(ModelMap modelMap) {
 
-        //List<User> list = userJpaRepository.findAll();
-        String message = "Hello World!";
-        modelMap.addAttribute("message", message);
+        List<User> list = userJpaRepository.findAll();
+        modelMap.addAttribute("list", list);
         return "home";
     }
 }
